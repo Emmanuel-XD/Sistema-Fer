4062cfd0b31d6d1d9c4e6a17e5aa4ab6d43ac27c
Emmanuel <55505148+Emmanuel-XD@users.noreply.github.com>
2022-12-18 12:54:58 -0600

    UpdateHistorial

--- views/historial.php
+++ views/historial.php
@@ -1,7 +1,16 @@
 <?php
 include_once "../includes/header.php";
-include_once "../includes/db.php";
+include_once "../includes/base.php";
+
+
+$sentencia = $base_de_datos->query("SELECT compras.total, compras.folio, compras.fecha, compras.id, compras.usuario,compras.departamento,
+GROUP_CONCAT(	articulos.codigo, '..',  articulos.descripcion,  '..', art_vendidos.cantidad, '..',  art_vendidos.precio  
+SEPARATOR '__') AS articulos FROM compras LEFT JOIN art_vendidos
+ON art_vendidos.id_compra = compras.id LEFT JOIN articulos ON
+ articulos.id = art_vendidos.id_articulo GROUP BY compras.id ORDER BY compras.id;");
+$compras = $sentencia->fetchAll(PDO::FETCH_OBJ);
 ?>
+
 
 <br>
 <br>
@@ -24,6 +33,7 @@ include_once "../includes/db.php";
 		<thead>
 			<tr class="bg-dark">
 				<th>ID</th>
+				<th>Folio</th>
 				<th>Fecha</th>
 				<th>Articulos</th>
 				<th>Total</th>
@@ -33,41 +43,45 @@ include_once "../includes/db.php";
 			</tr>
 		</thead>
 		<tbody>
+		<?php foreach ($compras as $compra) { ?>
 			
 				<tr>
-					<td><b>q</td></b>
-					<td>s</td>
+				<td><b><?php echo $compra->id ?></td></b>
+				<td><?php echo $compra->folio ?></td>
+					<td><?php echo $compra->fecha; ?></td>
 					<td>
 						<table class="table table-striped" id="table_id">
 							<thead>
 								<tr class="bg-dark">
 									<th>Codigo</th>
 									<th>Descripcion</th>
-                                    <th>Precio</th>
 									<th>Cantidad</th>
+                                    <th>Precio</th>
+								
 
 								</tr>
 							</thead>
 							<tbody>
-								
-							
+							<?php foreach (explode("__", $compra->articulos) as $productosConcatenados) {
+									$articulo = explode("..", $productosConcatenados)
+								?>
 									<tr>
-										<td>!</td>
-										<td>s</td>
-										<td>s</td>
-                                        <td>ss</td>
+										<td><?php echo $articulo[0] ?></td>
+										<td><?php echo $articulo[1] ?></td>
+										<td><?php echo $articulo[2] ?></td>
+										<td><?php echo '$'.$articulo[3] ?></td>
 									</tr>
-					
+								<?php } ?>
 							</tbody>
 						</table>
 					</td>
-					<td>s</td>
-				
-					<td><a class="btn btn-outline-danger" target="_blank" href="requisicion.php"><i class="fa fa-print"></i></a></td>
-					<td>ejemplo</td>
-                    <td>azur</td>
+					<td><?php echo  '$', $compra->total ?></td>
+					
+					<td><a class="btn btn-outline-danger" target="_blank" href="<?php echo "requisicion.php?id=" . $venta->id ?>"><i class="fa fa-print"></i></a></td>
+					<td><?php echo $compra->usuario ?></td>
+					<td><?php echo $compra->departamento ?></td>
 				</tr>
-	
+			<?php } ?>
 
 		</tbody>
 	</table>
