e7c501eed5e329ab7fbc91e5f8e7c6dea27674a7
Emmanuel <55505148+Emmanuel-XD@users.noreply.github.com>
2022-12-18 00:15:02 -0600

    NewFile
    Se agrego la vista del historial

---
+++ views/historial.php
@@ -0,0 +1,80 @@
+<?php
+include_once "../includes/header.php";
+include_once "../includes/db.php";
+?>
+
+<br>
+<br>
+<br>
+<br>
+<div class="col-xs-12">
+	<h1>Historial de compras</h1>
+	
+
+	<br>
+
+	<style>
+		.table thead th {
+			vertical-align: bottom;
+			border-bottom: 2px solid #dee2e6;
+			color: white;
+		}
+	</style>
+	<table class=" table table-striped" id="table_id">
+		<thead>
+			<tr class="bg-dark">
+				<th>ID</th>
+				<th>Fecha</th>
+				<th>Articulos</th>
+				<th>Total</th>
+				<th>PDF</th>
+				<th>Usuario</th>
+                <th>Departamento</th>
+			</tr>
+		</thead>
+		<tbody>
+			
+				<tr>
+					<td><b>q</td></b>
+					<td>s</td>
+					<td>
+						<table class="table table-striped" id="table_id">
+							<thead>
+								<tr class="bg-dark">
+									<th>Codigo</th>
+									<th>Descripcion</th>
+                                    <th>Precio</th>
+									<th>Cantidad</th>
+
+								</tr>
+							</thead>
+							<tbody>
+								
+							
+									<tr>
+										<td>!</td>
+										<td>s</td>
+										<td>s</td>
+                                        <td>ss</td>
+									</tr>
+					
+							</tbody>
+						</table>
+					</td>
+					<td>s</td>
+				
+					<td><a class="btn btn-outline-danger" target="_blank" href="requisicion.php"><i class="fa fa-print"></i></a></td>
+					<td>ejemplo</td>
+                    <td>azur</td>
+				</tr>
+	
+
+		</tbody>
+	</table>
+
+
+
+
+
+
+</div>
